a5e40be6085df5f10b6ca825d0c6d0c9979637b8
FreakyCoder <kurayogun@gmail.com>
2019-03-11 15:43:32 +0300

    README is integrated. Also, example is updated.

---
+++ README.md
@@ -0,0 +1,62 @@
+<img alt="React Native Custom Icon" src="https://github.com/WrathChaos/react-native-custom-icon/blob/master/assets/Screenshots/logo.png" width="1050"/>
+
+Custom Icon Lib for React Native.
+
+[![npm version](https://img.shields.io/npm/v/@freakycoder/react-native-custom-icon.svg)](https://www.npmjs.com/package/@freakycoder/react-native-custom-icon)
+[![npm](https://img.shields.io/npm/dt/@freakycoder/react-native-custom-icon.svg)](https://www.npmjs.org/package/@freakycoder/react-native-custom-icon)
+![Platform - Android and iOS](https://img.shields.io/badge/platform-Android%20%7C%20iOS-blue.svg)
+[![License: MIT](https://img.shields.io/badge/License-MIT-green.svg)](https://opensource.org/licenses/MIT)
+[![styled with prettier](https://img.shields.io/badge/styled_with-prettier-ff69b4.svg)](https://github.com/prettier/prettier)
+
+<p align="center">
+<img alt="React Native Custom Icon" src="https://github.com/WrathChaos/react-native-custom-icon/blob/master/assets/Screenshots/example.png" width="49.7%"/>
+</p>
+
+## Installation
+
+Add the dependency:
+
+### Pure React Native:
+
+```ruby
+npm i react-native-custom-icon
+```
+
+## Peer Dependencies
+
+##### IMPORTANT! You need install them.
+
+```
+"react": ">= 16.x",
+"react-native": ">= 0.55.x",
+"react-native-vector-icons": ">= 6.x.x"
+```
+
+## Basic Usage
+
+```ruby
+import MyIcon from "react-native-custom-icon";
+import IcomoonConfig from "./assets/icomoon/selection.json";
+<MyIcon name="fireworks" color="red" size={50} config={IcomoonConfig} />
+```
+
+### Configuration - Props
+
+
+| Property |  Type  | Default | Description                                                         |
+| -------- | :----: | :-----: | ------------------------------------------------------------------- |
+| name     | string |  null   | use this to set icon's name                                         |
+| color    | color  |  null   | use this to set icon's color                                        |
+| size     | number |   16    | use this to change icon's size                                      |
+| config   |  JSON  |  null   | must use this to provide which config and data to use as IconConfig |
+
+# Change Log
+
+
+## Author
+
+FreakyCoder, kurayogun@gmail.com
+
+## License
+
+React Native Custom Icon Library is available under the MIT license. See the LICENSE file for more info.
--- example/App.js
+++ example/App.js
@@ -1,23 +1,97 @@
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import {
+  Linking,
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+  View
+} from "react-native";
 import MyIcon from "react-native-custom-icon";
 import IcomoonConfig from "./assets/icomoon/selection.json";
+import { human, iOSColors, iOSUIKit } from "react-native-typography";
 
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
-});
+const title = "Welcome to \n" + "React Native Custom Icon!";
+const content =
+  "React Native Custom Icon is a library which making easier to use your own custom icons as font.\n" +
+  "It uses react-native-vector-icons library as peer dependency and you need to use Icomoon to \n";
++"create your own icons as .ttf font format. ";
+
+const instructions =
+  "Detailed information is on Medium blog\n" + "to get started: ";
+const instructions2 =
+  "Basic usage and installation is on Github\n" + "check README out: ";
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-        <MyIcon name="fireworks" color="red" size={50} config={IcomoonConfig} />
+        <Text style={styles.welcome}>{title}</Text>
+        <Text style={styles.instructions}>{content}</Text>
+        <View
+          style={{
+            alignItems: "center",
+            flexDirection: "column",
+            justifyContent: "center"
+          }}
+        >
+          <Text
+            style={{
+              ...iOSUIKit.largeTitleEmphasized,
+              fontSize: 25,
+              fontWeight: "bold"
+            }}
+          >
+            Medium Blog
+          </Text>
+          <Text style={styles.instructions}>{instructions}</Text>
+          <TouchableOpacity
+            onPress={() => {
+              Linking.openURL("https://www.google.com");
+            }}
+          >
+            <MyIcon
+              name="medium-logo"
+              color="purple"
+              size={45}
+              config={IcomoonConfig}
+            />
+          </TouchableOpacity>
+        </View>
+        <Text
+          style={{
+            ...iOSUIKit.largeTitleEmphasized,
+            top: 12,
+            fontSize: 25,
+            fontWeight: "bold"
+          }}
+        >
+          Github README
+        </Text>
+        <Text style={styles.instructions}>{instructions2}</Text>
+        <TouchableOpacity
+          onPress={() => {
+            Linking.openURL(
+              "https://www.github.com/WrathChaos/react-native-custom-icon"
+            );
+          }}
+        >
+          <View style={{ flexDirection: "row" }}>
+            <MyIcon
+              name="github-logo"
+              color="#7E11FF"
+              size={35}
+              config={IcomoonConfig}
+            />
+            <View style={{ left: 8 }}>
+              <MyIcon
+                name="github"
+                color="#7E11FF"
+                size={35}
+                config={IcomoonConfig}
+              />
+            </View>
+          </View>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -26,18 +100,21 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    paddingTop: "30%",
     alignItems: "center",
     backgroundColor: "#F5FCFF"
   },
   welcome: {
-    fontSize: 20,
+    ...iOSUIKit.largeTitleEmphasized,
+    fontSize: 26,
     textAlign: "center",
     margin: 10
   },
   instructions: {
+    ...human.footnote,
+    color: iOSColors.gray,
+    padding: 8,
     textAlign: "center",
-    color: "#333333",
     marginBottom: 5
   }
 });
